eb39a7e107aa1f2db2797527fd2caf70aa92ccbf
Thomas Johnson <thomasij813@gmail.com>
2019-06-01 19:36:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,10 @@
 function theBeatlesPlay(musicians, instruments) {
   const outputArr = []
   for (var i = 0; i < musicians.length; i++) {
-    const musician = musician[i]
+    const musician = musicians[i]
     const instrument = instrument[i]
     outputArr.push(`${musician} plays ${instrument}`)
   }
   console.log(outputArr)
   return outputArr
 }
-
-console.log(theBeatlesPlay(['thomas'], ['piano']))
