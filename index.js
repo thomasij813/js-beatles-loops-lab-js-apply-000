b8666f179c816aa495cdbdea35552d4e60f021e5
Thomas Johnson <thomasij813@gmail.com>
2019-06-01 19:32:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  
+  return musicians.map(function(musician, i) {
+    return `${musician} plays the ${instruments[i]}`
+  })
 }
