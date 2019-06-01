55b0cec2f43ee4c245428c21ca39904aca341e97
Thomas Johnson <thomasij813@gmail.com>
2019-06-01 19:36:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments) {
   const outputArr = []
   for (var i = 0; i < musicians.length; i++) {
     const musician = musicians[i]
-    const instrument = instrument[i]
+    const instrument = instruments[i]
     outputArr.push(`${musician} plays ${instrument}`)
   }
   return outputArr
